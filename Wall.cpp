7c5175dcf4334f3049c3ac77fbac47b78ba4cb6a
dunlapc4 <conor4@pdx.edu>
2023-11-25 17:29:26 -0800

    made wall class and constructors and destrctr and draw func

--- Wall.cpp
+++ Wall.cpp
@@ -1,10 +1,35 @@
-#include <FL/gl.h>
-#include <FL/glu.h>
+#include "Wall.h"
+Wall::~Wall() {
+	if (initialized) {
+		glDeleteLists(displayList, 1);
+		gluDeleteQuadric(qobj);
+	}
+}
+
+Wall::Wall(GLdouble theHeight, GLdouble theWidth, GLdouble theLength, float x, float y, float z) {
+	displayList = 0;
+	initialized = false;
+	height = theHeight;
+	length = theLength;
+	width = theWidth;
+	coord[0] = x;
+	coord[1] = y;
+	coord[2] = z;
+}
 
 
-class Wall{
-private:
-	//
-public:
-	//
-};
+
+bool Wall::Initialize() {
+	//code goes here 
+	initialized = true;
+	return true;
+}
+
+void Wall::Draw() {
+	//draw the wall object
+	glPushMatrix();
+	glTranslatef(coord[0], coord[1], coord[2]);
+	glCallList(displayList);
+	glPopMatrix();
+}
+
--- Wall.h
+++ Wall.h
@@ -5,7 +5,19 @@
 class Wall {
 private:
 	GLubyte displayList;
+	GLdouble height;
+	GLdouble width;
+	GLuint  texture_obj;    // The object for the wall texture 
+	bool    initialized;    // Whether or not we have been initialize
+	GLdouble length;
+	float coord[3];
+	GLUquadric* qobj;
 
 public:
-	//
+	Wall() {displayList 0; initialized = false; height = 20.0; width = 20.0; length = 20.0; coord[0] = coord[1] = coord[2] = 0.0f}
+	Wall(GLdouble theHeight, GLdouble theWidth, GLdouble theLength, float x, float y, float z);
+	~Wall();
+
+	bool Initialize();
+	void Draw();
 };
